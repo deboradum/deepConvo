36eb9ddd8d2f10598caac41e37ac0fb9b627149f
deboradum <pvwpvw@protonmail.com>
2024-06-23 10:48:00 +0200

    writing to file

--- Conversationalist.py
+++ Conversationalist.py
@@ -1,6 +1,14 @@
+import json
+
+# Writes dict_to_write to a jsonl file.
+def write_file(filepath, dict_to_write):
+    with open(filepath, "a+") as f:
+        json_line = json.dumps(dict_to_write)
+        f.write(json_line + "\n")
+
 
 class Conversationalist:
-    def __init__(self, llm, global_history, role, system_prompt, verbose):
+    def __init__(self, llm, global_history, role, system_prompt, filepath, verbose=False):
         self.llm = llm
         self.global_history = global_history
         # Role is the name/ type of the roleplay character this Conversationalist
@@ -8,10 +16,14 @@ class Conversationalist:
         self.role = role
         self.history = [{"role": "system", "content": system_prompt}]
         self.verbose = verbose
+        self.filepath = filepath
 
     def talk(self):
         resp = self.llm.run(self.history)
-        self.global_history.append({"role": self.role, "content": resp})
+        global_history_entry = {"role": self.role, "content": resp}
+        self.global_history.append(global_history_entry)
+        write_file(self.filepath, global_history_entry)
+
         self.history.append({"role": "assistant", "content": resp})
 
         if self.verbose:
